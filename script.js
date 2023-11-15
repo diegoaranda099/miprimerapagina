218e54117fb1023e51af7792961813b64f840ac4
diegoaranda099 <diegoaranda99@gmail.com>
2023-11-14 23:25:42 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/* let nombre = "diego";
+console.log(nombre);
+alert("Bienvenido a mi primera web"); */
+let contenidoTitulo ="nombre";
+let titulo = document.querySelector(".logo .fuente-acento");
+
+
+
